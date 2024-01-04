5849d8c49f4aa79caab65688e9ad59cea41bb3d0
vuotlenchinhminh94 <vuotlenchinhminh171094@gmail.com>
2024-01-04 10:29:45 +0700

    created table user

--- src/migrations/20240103152522-create-user.js
+++ src/migrations/20240103152522-create-user.js
@@ -9,13 +9,22 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      email: {
+        type: Sequelize.STRING
+      },
       firstName: {
         type: Sequelize.STRING
       },
       lastName: {
         type: Sequelize.STRING
       },
-      email: {
+      address: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.BOOLEAN
+      },
+      roleId: {
         type: Sequelize.STRING
       },
       createdAt: {
--- src/models/user.js
+++ src/models/user.js
@@ -14,9 +14,13 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
+    // id: DataTypes.INTEGER,
+    email: DataTypes.STRING,
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+    address: DataTypes.STRING,
+    gender: DataTypes.BOOLEAN,
+    roleId: DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'User',
